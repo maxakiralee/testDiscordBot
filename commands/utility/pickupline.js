9961b69ca9a60972d40b7fabc0eb742b914129bc
Max <maxlee10@g.ucla.edu>
2024-04-20 08:37:16 -0700

    gemini api integrated for pickupline

--- commands/utility/pickupline.js
+++ commands/utility/pickupline.js
@@ -1,3 +1,6 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI("AIzaSyB-I7Y390kDYmlAgk90JWZSZkm5Sbbk_iQ");
+const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
 const { SlashCommandBuilder } = require('discord.js');
 
@@ -6,11 +9,19 @@ const wait = require('node:timers/promises').setTimeout;
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('pickline')
-		.setDescription('Trying to find love? Well Andy gotchu!'),
+		.setDescription('Trying to find love? I got U!'),
 	async execute(interaction) {
+
+        const prompt = "Give a witty line for someone you love."
+      
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        await interaction.reply(response.text());
+        /*
 		await interaction.reply('Pong!');
             await wait(4_000);
             await interaction.editReply('BRO!');
+            */
         //await interaction.reply({ content: "Is your name Google? Because you have everything Ive been searching for." });
 	},
 };
